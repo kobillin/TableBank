ad1953514c2f97ba5ec56fa497459d8f76039f2a
KipsangA <40466099+KipsangA@users.noreply.github.com>
2020-03-01 13:09:32 +0300

    First commit

---
+++ app/src/main/java/com/example/tablebank/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tablebank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
